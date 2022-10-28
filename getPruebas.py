f378d719aa7e707446fe9a91a7b55c37abeadb77
Jazz <99265919+Desarrollo5HQ5@users.noreply.github.com>
2022-10-28 15:02:38 -0500

    a

--- getPruebas.py
+++ getPruebas.py
@@ -1,8 +1,15 @@
 import requests
 import json
-parameters_= {
-    "ClientSecret":"fdaf31a7-d9b6-461b-bf6c-a834d807aa78",
-    "ClientId": "e962f0a3-c4ae-4d62-8663-123cd3615ce6"
-     }  
-response_=requests.post("https://api.psicoweb.com/login", data = parameters_)
-print(json.loads(response_.text)["data"]["token"])
+
+def token_():
+    parameters_= {
+        "ClientSecret":"fdaf31a7-d9b6-461b-bf6c-a834d807aa78",
+        "ClientId": "e962f0a3-c4ae-4d62-8663-123cd3615ce6"
+        }  
+    response_=requests.post("https://api.psicoweb.com/login", data = parameters_)
+    return json.loads(response_.text)["data"]["token"]
+
+autorizacion = {"Authorization": token_()}
+
+descargable = requests.get("https://api.psicoweb.com/v1/aplicaciones/link/ExcelConcentrado?prueba=NIC&cedula=53102572req18332&puesto=NICVal", headers = autorizacion)
+print( descargable.text)
