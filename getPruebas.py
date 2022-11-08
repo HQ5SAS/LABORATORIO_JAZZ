36274b8752563522b47532630784c2d35a93d725
Jazz <99265919+Desarrollo5HQ5@users.noreply.github.com>
2022-11-08 15:20:26 -0500

    get pruebas y estandarizar

--- getPruebas.py
+++ getPruebas.py
@@ -18,11 +18,11 @@ listaCar= []
 # listaPrubas=["Cleaver","PVC","Valores","NEC","NIC","NOC","FP16","Terman","Zavic"]
 # descargable = requests.get("https://api.psicoweb.com/v1/Puestos", headers = autorizacion)# --> para obtener el nombre de los cargos existentes(buscar "nombre")
 #descargable = requests.get("https://api.psicoweb.com/v1/aplicaciones/link/ExcelConcentrado?prueba=NEC&puesto=NECNOCTerVal", headers = autorizacion) -->Para obtner por puesto las pruebas
-descargable = requests.get("https://api.psicoweb.com/v1/aplicaciones/link/ExcelConcentrado?prueba=Terman&puesto=NECNOCTerVal", headers = autorizacion)
+descargable = requests.get("https://api.psicoweb.com/v1/aplicaciones/link/ExcelConcentrado?prueba=Valores&puesto=NECNOCTerVal", headers = autorizacion)
 infoCargos= json.loads(descargable.text).get("data")
 linkdes=infoCargos.get("link")
 resp = requests.get(linkdes)
-open("test.xlsx", "wb").write(resp.content)
+open("C:\\Users\\CO-166\\Desktop\\pruebas combinaciones\\test.xlsx", "wb").write(resp.content)
 
 # for x in infoCargos:
 #     var=x["nombre"]
