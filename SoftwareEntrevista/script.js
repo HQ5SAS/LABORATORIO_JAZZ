46d1f2d783b9d2ec8b2bd1b1e79acb34a664de07
Jazz <99265919+Desarrollo5HQ5@users.noreply.github.com>
2022-11-29 17:50:29 -0500

    preguntas añadidas

--- SoftwareEntrevista/script.js
+++ SoftwareEntrevista/script.js
@@ -1,16 +1,34 @@
-const videoButton= document.getElementById('video_bttn');
+const videoButton= document.getElementById('next_bttn');
 const video = document.getElementById('video_');
+const texto =document.getElementById('preguna_txt');
+var preguntas=[
+    '1.Háblame de ti',
+    '2.¿Qué te gusta hacer en tu tiempo libre?',
+    '3.¿Por qué te interesa el puesto?',
+    '4.¿Por qué dejaste tu anterior empleo? ¿Por qué quieres cambiar de empleo?',
+    '5.Cuéntame de algún momento de tu vida laboral en el que hayas cometido un error, ¿cómo lo solucionaste?',
+    '6.¿Cuál es tu mayor virtud o habilidad?',
+    '7.¿Cuál es tu mayor debilidad o defecto?',
+    '8.¿Por qué deberíamos contratarte?'
+]
+
+
 
 let mediaRecorder;
 
 videoButton.onclick=()=>{
     console.log(videoButton.textContent);
+    cou
     switch(videoButton.textContent){
-        case 'Grabar':
-            videoButton.textContent ='Detener';
+        case 'Listo':
+            videoButton.textContent ='Siguiente';
             startRecording();
             break;
-        case 'Detener':
+        case 'Siguiente':
+            videoButton.textContent='Grabar';
+            stopRecording();
+            break;       
+        case 'Finalizar':
             videoButton.textContent='Grabar';
             stopRecording();
             break;    
